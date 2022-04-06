e83a770275ca3d689c00a0e30ff7f6a82e601ebc
Gaboxz <80337191+Gaboxz@users.noreply.github.com>
2022-04-05 20:49:44 -0400

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Scripts/Isaac/SetPlayerPrefs.cs
+++ Assets/Scripts/Isaac/SetPlayerPrefs.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class SetPlayerPrefs : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
 
+
+    //    PlayerPrefs.SetInt("CargarMenu 1", 1);
+
+    //    PlayerPrefs.SetInt("CargarMenu 2", 0);
+    //    PlayerPrefs.SetInt("CargarMenu 3", 0);
+    //    PlayerPrefs.SetInt("CargarMenu 4", 0);
+
+    //}
+
+    public void RestartLevels()
+    {
 
         PlayerPrefs.SetInt("CargarMenu 1", 1);
 
         PlayerPrefs.SetInt("CargarMenu 2", 0);
         PlayerPrefs.SetInt("CargarMenu 3", 0);
         PlayerPrefs.SetInt("CargarMenu 4", 0);
-
     }
 
 
